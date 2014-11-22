868ccda9ef97d94eab144864493c22c09d3ce75d
Ben <ben@pickledbrain.co>
2014-11-22 11:05:05 -0700

    Setup crate prefab. Added basic tank movement

---
+++ NetTank/Assets/Scripts/TankMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMovement : MonoBehaviour 
+{
+	public float speed = 10f;
+	public float rotationsSpeed = 10f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position += Input.GetAxis("Vertical") * transform.forward * 
+			speed * Time.deltaTime;
+	
+		float rotation  = Input.GetAxis("Horizontal") * 
+			rotationsSpeed * Time.deltaTime;
+
+		transform.Rotate(Vector3.up,rotation);
+
+		if(Input.GetAxis("Fire1") > 0.5f)
+		{
+			//Fire Weapon
+		}
+	}
+}
