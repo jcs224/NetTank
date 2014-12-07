8b04bc0cd8f711487c65dd6863442e9eaf003f61
Ben <ben@pickledbrain.co>
2014-12-07 16:26:32 -0700

    Added NetDebug Script. Setup damagable scripts on tanks along with bullet collisions.

---
+++ NetTank/Assets/Scripts/NetDebug.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NetworkView))]
+public class NetDebug : MonoBehaviour 
+{
+    private TextMesh textMesh;
+    private string output;
+    private Damagable damage;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject textGo = new GameObject("TextObject");
+        textGo.transform.parent = this.transform;
+        textGo.transform.localPosition = new Vector3(0, 1f, 0);
+        textGo.transform.localScale = Vector3.one * 0.1f;
+        textGo.transform.Rotate(Vector3.up * 180f);
+
+        textGo.AddComponent<MeshRenderer>();
+
+        textMesh = textGo.AddComponent<TextMesh>();
+        textMesh.anchor = TextAnchor.UpperLeft;
+        
+
+        Font font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+
+        textGo.renderer.material = font.material;
+        textMesh.font = font;
+
+        textMesh.fontSize = 30;
+        textMesh.fontStyle = FontStyle.Bold;
+        
+
+
+
+        damage = GetComponent<Damagable>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        if(damage != null)
+            textMesh.text = networkView.viewID + "\n" + (networkView.isMine ? "Mine" : "NotMine") + "\nDamage: " + damage.health + "\n " + output;
+        else
+            textMesh.text = networkView.viewID + "\n" + (networkView.isMine ? "Mine" : "NotMine") + "\n " + output;
+	}
+}
--- NetTank/Assets/Scripts/TankMovement.cs
+++ NetTank/Assets/Scripts/TankMovement.cs
@@ -9,10 +9,12 @@ public class TankMovement : MonoBehaviour
 	public GameObject bulletPrefab;
 	public Transform bulletFirePosition;
 
+    private Damagable damage;
+         
 	
 	void Start () 
 	{
-	
+        damage = GetComponent<Damagable>();
 	}
 
 
@@ -28,20 +30,49 @@ public class TankMovement : MonoBehaviour
             {
                 //Fire Weapon
                 //GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, bulletFirePosition.position, transform.rotation);
-                Network.Instantiate(bulletPrefab, bulletFirePosition.position, transform.rotation,0);
+                //Network.Instantiate(bulletPrefab, bulletFirePosition.position, transform.rotation, 0);
+
+                //Allocate a new network id and create the new bullet with this id
+                networkView.RPC("CreateBullet", RPCMode.All, Network.AllocateViewID());
             }
+
+
+            if (damage.isDead())
+            {
+                Network.Destroy(gameObject);
+            }
+
+
         }
     }
 
+    [RPC]
+    private void CreateBullet(NetworkViewID id)
+    {
+        GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, bulletFirePosition.position, transform.rotation);
+        bulletGo.networkView.viewID = id;
+    }
 
 
 	void FixedUpdate()
 	{
 		if (networkView.isMine)
 		{
-			rigidbody.AddForce( (transform.forward * thrust) * Input.GetAxis("Vertical") );
+			//rigidbody.AddForce( (transform.forward * thrust) * Input.GetAxis("Vertical") );
+            rigidbody.velocity = transform.forward * thrust * Input.GetAxis("Vertical");
 		}
 	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (networkView.isMine)
+        {
+            if (col.tag == "Bullet")
+            {
+                damage.hurt(1);
+            }
+        }
+    }
 }
 
 
